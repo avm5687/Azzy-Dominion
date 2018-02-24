b6e9dda3514e1e0e3f5d869a8836829d454e5f8d
Azzy <azzymcalpine@gmail.com>
2018-02-24 12:02:22 -0500

    more homework

--- C430/query.sql
+++ C430/query.sql
@@ -7,11 +7,11 @@ from Student
 where studentnum in
 	(select studentnum
 		from Taking, Class
-		where semester='Spring 2018' and Taking.studentnum=Student.studentnum and department='CMPSC' and num=430);
+		where Taking.semester='Spring 2018' and Taking.studentnum=Student.studentnum and department='CMPSC' and num=430);
 
 select name, standing
 from Student natural join Taking
-where schedulenum
+where schedulenum in
 	(select schedulenum
 	from Teaches
 	where name='Linda Null' or name='Jeremy Blum');
