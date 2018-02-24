d30e84d412683477ab210f456a9478406b7d9f58
Azzy <azzymcalpine@gmail.com>
2018-02-24 03:13:32 -0500

    homework4

--- C430/query.sql
+++ C430/query.sql
@@ -1,4 +1,4 @@
 select name
 from Student, Taking, Class
-where semester=Spring2018 and Taking.studentnum=Student.studentnum and department=CMPSC and num=430;
+where semester='Spring 2018' and Taking.studentnum=Student.studentnum and department='CMPSC' and num=430;
 
