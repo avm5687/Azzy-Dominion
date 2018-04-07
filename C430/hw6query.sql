47bc85a94d49fc68a62c94affedea74925841c87
avm5687 <azzymcalpine@gmail.com>
2018-04-06 22:35:01 -0400

    430

--- C430/hw6query.sql
+++ C430/hw6query.sql
@@ -6,4 +6,14 @@ where department = 'CMPSC' and num= 463);
 
 
 update Taking
-	set grade = 3.2;
+	set grade = case
+				when grade >= 2.0 then 1.0
+				end;
+
+update Taking
+	set grade = 2.0;
+
+update Taking
+	set grade = case
+		when grade >= 3.0 then 1.5
+		end;
